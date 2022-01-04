e59589542ef133c758389877d50a2bc6ef7e7454
Josano Pereira <josanojp@gmail.com>
2022-01-04 14:49:12 +0100

    atc

--- Progresso/usuario/models.py
+++ Progresso/usuario/models.py
@@ -6,6 +6,6 @@ from django.contrib.auth.models import User
 
 class Perfil(models.Model):
     nome_completo = models.CharField(max_length=50, null=True)
-    bi = models.CharField(max_length=14, null=True)
+    bi = models.CharField(max_length=14, null=True, verbose_name='BI')
     telefone = models.CharField(max_length=16, null=True)
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
