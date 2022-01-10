6c2600fda20bbc1ed2cd777cb26f81dddaddfe5f
Josano Pereira <josanojp@gmail.com>
2022-01-10 11:47:09 +0000

    act

--- Progresso/usuario/models.py
+++ Progresso/usuario/models.py
@@ -1,21 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-<<<<<<< Updated upstream
-
-
-=======
->>>>>>> Stashed changes
 # Create your models here.
+
 
 class Perfil(models.Model):
     nome_completo = models.CharField(max_length=50, null=True)
-<<<<<<< Updated upstream
     bi = models.CharField(max_length=14, null=True, verbose_name='BI')
     telefone = models.CharField(max_length=16, null=True)
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
-=======
-    bi = models.CharField(max_length=14, null=True)
-    telefone = models.CharField(max_length=13, null=True)
-    #usuario = models.OneToOneField(User, on_delete=models.PROTECT(collector=True, field=True, sub_objs=User, using=True))
-
->>>>>>> Stashed changes
